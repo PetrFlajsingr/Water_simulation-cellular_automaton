50008ee156939901f2512645341a403d211c2a1f
PetrFlajsingr <xflajs00@stud.fit.vutbr.cz>
2019-12-15 14:33:47 +0100

    build fix

--- misc/Utilities.h
+++ misc/Utilities.h
@@ -5,6 +5,7 @@
 #ifndef GMU_FLUID_SIMULATION_CELLULAR_AUTOMATA_UTILITIES_H
 #define GMU_FLUID_SIMULATION_CELLULAR_AUTOMATA_UTILITIES_H
 
+#include <string>
 class Utilities {
 public:
   static std::string readFile(const std::string &filename);
