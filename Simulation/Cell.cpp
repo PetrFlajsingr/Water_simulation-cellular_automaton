b102a94b342b4c771085dac23365cb58ecc39dd5
Igor Frank <ifrank@seznam.cz>
2019-12-16 23:14:34 +0100

    added solidVolume field

--- Simulation/Cell.cpp
+++ Simulation/Cell.cpp
@@ -4,8 +4,11 @@
 
 #include "Cell.h"
 
-Cell::Cell(float fluidVolume, uint32_t stable) : fluidVolume(fluidVolume), fluidHorizontalLefover(0.0), stable(stable) {}
+Cell::Cell(float fluidVolume, uint32_t stable, float solidVolume)
+    : fluidVolume(fluidVolume), fluidHorizontalLefover(0.0), stable(stable), solidVolume(solidVolume) {}
 
 void Cell::setFluidVolume(float fluidVolume) { Cell::fluidVolume = fluidVolume; }
 
 void Cell::setStable(uint32_t stable) { Cell::stable = stable; }
+
+void Cell::setSolidVolume(float solidVolume) { Cell::solidVolume = solidVolume; }
--- Simulation/Cell.h
+++ Simulation/Cell.h
@@ -11,12 +11,17 @@ class Cell {
 public:
   void setFluidVolume(float fluidVolume);
 
+  void setSolidVolume(float solidVolume);
+
   void setStable(uint32_t stable);
 
-  explicit Cell(float fluidVolume = 0.0, uint32_t stable = 0);
+  explicit Cell(float fluidVolume = 0.0, uint32_t stable = 0, float solidVolume = 0.0);
 
 private:
   float fluidVolume;
+  float solidVolume;
+
+private:
   float fluidHorizontalLefover;
   uint32_t stable;
 };
