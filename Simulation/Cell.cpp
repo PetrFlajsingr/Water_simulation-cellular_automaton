1916f3fecdb8b7f74d4bae7a0ad0167853e56a9b
Igor Frank <ifrank@seznam.cz>
2019-12-30 18:08:06 +0100

    refactor

--- Simulation/Cell.cpp
+++ Simulation/Cell.cpp
@@ -12,6 +12,6 @@ Cell::Cell(float fluidVolume, glm::vec4 velocity)/* : fluidVolume(fluidVolume), 
 void Cell::setFluidVolume(float fluidVolume) { Cell::velocityFluidVolume.w = fluidVolume; }
 
 std::ostream &operator<<(std::ostream &out, const Cell &cell) {
-    out << " Fluid: " << cell.velocityFluidVolume.w << "Velocity: " << cell.velocityFluidVolume.x << " " << cell.velocityFluidVolume.y << " " << cell.velocityFluidVolume.z;
+    out << " Fluid: " << cell.velocityFluidVolume.w << " Velocity: " << cell.velocityFluidVolume.x << " " << cell.velocityFluidVolume.y << " " << cell.velocityFluidVolume.z;
     return out;
 }
