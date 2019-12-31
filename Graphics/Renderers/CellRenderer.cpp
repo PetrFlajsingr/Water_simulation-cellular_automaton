0da6e37bea7ab6bca4acc41865575faeb67f04cf
Igor Frank <ifrank@seznam.cz>
2019-12-31 11:56:07 +0100

    Revert "added method changer"
    This reverts commit 26f5e090

--- Graphics/Renderers/CellRenderer.cpp
+++ Graphics/Renderers/CellRenderer.cpp
@@ -22,8 +22,7 @@ CellRenderer::CellRenderer([[maybe_unused]] const std::string &cellModelPath, co
     : projectionMat(projectionMat), tankSize(tankSize) {
   using namespace ShaderLiterals;
   setShaderLocation(SRC_DIR + "/Shaders"s);
-  basicProgram = std::make_shared<Program>("basic"_vert, "basic"_frag, "basic"_geom);
-  velocityProgram = std::make_shared<Program>("basic"_vert, "basic"_frag, "velocity"_geom);
+  program = std::make_shared<Program>("basic"_vert, "basic"_frag, "basic"_geom);
   std::vector<glm::vec4> positions{glm::compMul(tankSize), glm::vec4(-1)};
 
   vbo = createBuffer(std::vector<Model::VertexData>{
@@ -35,44 +34,23 @@ CellRenderer::CellRenderer([[maybe_unused]] const std::string &cellModelPath, co
 
 float t = 0;
 void CellRenderer::draw(const glm::mat4 &viewMat, const glm::vec3 &cameraPos, const BufferPtr &cellBuffer,
-                        const BufferPtr &cellInfoBuffer, unsigned int simulationType, float cellSize) {
+                        const BufferPtr &cellInfoBuffer, float cellSize) {
+  program->use();
 
-  if (simulationType == 0) {
-    basicProgram->use();
+  vao->bind();
+  cellBuffer->bindBase(GL_SHADER_STORAGE_BUFFER, 1);
+  cellInfoBuffer->bindBase(GL_SHADER_STORAGE_BUFFER, 2);
+  program->setMatrix4fv("Model", glm::value_ptr(glm::mat4(1.0)), 1, GL_FALSE);
+  program->setMatrix4fv("View", glm::value_ptr(viewMat), 1, GL_FALSE);
+  program->setMatrix4fv("Projection", glm::value_ptr(projectionMat), 1, GL_FALSE);
+  program->set("cellSize", cellSize);
+  program->set("time", t);
+  program->set3uiv("tankSize", glm::value_ptr(tankSize), 1);
+  program->set3fv("cameraPosition", glm::value_ptr(cameraPos));
+  glDrawArraysInstanced(GL_POINTS, 0, 1, glm::compMul(tankSize));
+  cellBuffer->unbindBase(GL_SHADER_STORAGE_BUFFER, 1);
+  cellInfoBuffer->unbindBase(GL_SHADER_STORAGE_BUFFER, 2);
+  vao->unbind();
 
-    vao->bind();
-    cellBuffer->bindBase(GL_SHADER_STORAGE_BUFFER, 1);
-    cellInfoBuffer->bindBase(GL_SHADER_STORAGE_BUFFER, 2);
-    basicProgram->setMatrix4fv("Model", glm::value_ptr(glm::mat4(1.0)), 1, GL_FALSE);
-    basicProgram->setMatrix4fv("View", glm::value_ptr(viewMat), 1, GL_FALSE);
-    basicProgram->setMatrix4fv("Projection", glm::value_ptr(projectionMat), 1, GL_FALSE);
-    basicProgram->set("cellSize", cellSize);
-    basicProgram->set("time", t);
-    basicProgram->set3uiv("tankSize", glm::value_ptr(tankSize), 1);
-    basicProgram->set3fv("cameraPosition", glm::value_ptr(cameraPos));
-    glDrawArraysInstanced(GL_POINTS, 0, 1, glm::compMul(tankSize));
-    cellBuffer->unbindBase(GL_SHADER_STORAGE_BUFFER, 1);
-    cellInfoBuffer->unbindBase(GL_SHADER_STORAGE_BUFFER, 2);
-    vao->unbind();
-  } else {
-
-    velocityProgram->use();
-
-    vao->bind();
-    cellBuffer->bindBase(GL_SHADER_STORAGE_BUFFER, 1);
-    cellInfoBuffer->bindBase(GL_SHADER_STORAGE_BUFFER, 2);
-    velocityProgram->setMatrix4fv("Model", glm::value_ptr(glm::mat4(1.0)), 1, GL_FALSE);
-    velocityProgram->setMatrix4fv("View", glm::value_ptr(viewMat), 1, GL_FALSE);
-    velocityProgram->setMatrix4fv("Projection", glm::value_ptr(projectionMat), 1, GL_FALSE);
-    velocityProgram->set("cellSize", cellSize);
-    velocityProgram->set("time", t);
-    velocityProgram->set3uiv("tankSize", glm::value_ptr(tankSize), 1);
-    velocityProgram->set3fv("cameraPosition", glm::value_ptr(cameraPos));
-    glDrawArraysInstanced(GL_POINTS, 0, 1, glm::compMul(tankSize));
-    cellBuffer->unbindBase(GL_SHADER_STORAGE_BUFFER, 1);
-    cellInfoBuffer->unbindBase(GL_SHADER_STORAGE_BUFFER, 2);
-    vao->unbind();
-
-  }
-  t += 0.001;
+  t+=0.001;
 }
