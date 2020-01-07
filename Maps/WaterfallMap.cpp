1be4439a7f8dcaa41b521e5826f46f20748727bf
Igor Frank <ifrank@seznam.cz>
2020-01-07 15:58:23 +0100

    GMU_water_simulation_cellular_automata-2 Test terrains
    waterfall indices fix

--- Maps/WaterfallMap.cpp
+++ Maps/WaterfallMap.cpp
@@ -7,21 +7,21 @@
 void WaterfallMap::setup(SimulationCompute &simulation, glm::uvec3 tankSize) {
   loc_assert(glm::all(glm::equal(tankSize, glm::uvec3(32))), "Selected map does not match tank size!");
 
-  simulation.setCells({{23, 48, 0}, {24, 48, 0}, {25, 48, 0}, {26, 48, 0}, {27, 48, 0}}, CellFlags::Solid);
+  simulation.setCells({{14, 30, 0}, {15, 30, 0}, {16, 30, 0}, {17, 30, 0}, {18, 30, 0}}, CellFlags::Solid);
   {
     std::vector<glm::uvec3> result{};
     for (auto z : MakeRange::range(10)) {
-      result.emplace_back(glm::uvec3{20, 0, z});
-      result.emplace_back(glm::uvec3{20, 1, z});
-      result.emplace_back(glm::uvec3{30, 0, z});
-      result.emplace_back(glm::uvec3{30, 1, z});
+      result.emplace_back(glm::uvec3{5, 0, z});
+      result.emplace_back(glm::uvec3{5, 1, z});
+      result.emplace_back(glm::uvec3{25, 0, z});
+      result.emplace_back(glm::uvec3{25, 1, z});
     }
-    for (auto x : MakeRange::range(20, 30)) {
+    for (auto x : MakeRange::range(5, 25)) {
       result.emplace_back(glm::uvec3{x, 0, 10});
       result.emplace_back(glm::uvec3{x, 1, 10});
     }
     simulation.setCells(result, CellFlags::Solid);
-    simulation.setCells({{23, 49, 0}, {24, 49, 0}, {25, 49, 0}, {26, 49, 0}, {27, 49, 0}}, CellFlags::FluidSource);
+    simulation.setCells({{14, 31, 0}, {15, 31, 0}, {16, 31, 0}, {17, 31, 0}, {18, 31, 0}}, CellFlags::FluidSource);
   }
 }
 void WaterfallMap::operator()(SimulationCompute &simulation, BaseMap::Time time) {}
