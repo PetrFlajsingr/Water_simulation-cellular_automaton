75cfd7e4393165118efd5738ddbdfec4a77935dc
Igor Frank <ifrank@seznam.cz>
2020-01-07 17:20:01 +0100

    GMU_water_simulation_cellular_automata-2 Test terrains
    added floor

--- Maps/WaterfallMap.cpp
+++ Maps/WaterfallMap.cpp
@@ -8,21 +8,14 @@ void WaterfallMap::setup(SimulationCompute &simulation, glm::uvec3 tankSize) {
   loc_assert(glm::all(glm::equal(tankSize, glm::uvec3(32))), "Selected map does not match tank size!");
 
   simulation.setCells({{14, 30, 0}, {15, 30, 0}, {16, 30, 0}, {17, 30, 0}, {18, 30, 0}}, CellFlags::Solid);
-  {
-    std::vector<glm::uvec3> result{};
-    for (auto z : MakeRange::range(10)) {
-      result.emplace_back(glm::uvec3{5, 0, z});
-      result.emplace_back(glm::uvec3{5, 1, z});
-      result.emplace_back(glm::uvec3{25, 0, z});
-      result.emplace_back(glm::uvec3{25, 1, z});
-    }
-    for (auto x : MakeRange::range(5, 25)) {
-      result.emplace_back(glm::uvec3{x, 0, 10});
-      result.emplace_back(glm::uvec3{x, 1, 10});
-    }
-    simulation.setCells(result, CellFlags::Solid);
-    simulation.setCells({{14, 31, 0}, {15, 31, 0}, {16, 31, 0}, {17, 31, 0}, {18, 31, 0}}, CellFlags::FluidSource);
-  }
+  simulation.setRangeCells(MakeRange::range<unsigned int, 3>({0, 0, 0}, {tankSize.x, 1, tankSize.z}, {1, 1, 1}),
+                           CellFlags::Solid);
+  simulation.setRangeCells(MakeRange::range<unsigned int, 3>({0, 1, 0}, {tankSize.x, 5, tankSize.z}, {1, 1, tankSize.z - 1}),
+                           CellFlags::Solid);
+  simulation.setRangeCells(MakeRange::range<unsigned int, 3>({0, 1, 0}, {tankSize.x, 5, tankSize.z}, {tankSize.x - 1, 1, 1}),
+                           CellFlags::Solid);
+  simulation.setCells({{14, 31, 0}, {15, 31, 0}, {16, 31, 0}, {17, 31, 0}, {18, 31, 0}}, CellFlags::FluidSource);
+
 }
 void WaterfallMap::operator()(SimulationCompute &simulation, BaseMap::Time time) {}
 
